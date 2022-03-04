25380ca38409637bd5f27e26a659fce929195398
EvaCherkashyna <eva.cherkashyna2108@gmail.com>
2022-03-04 14:01:43 +0200

    add blank project

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+import { Container } from './Container'
+import {IoMoon, IoMoonOutline} from 'react-icons/io5'
+export const Header= ()=>{
+    return(
+        <div>
+
+
+        </div>
+    )
+}
